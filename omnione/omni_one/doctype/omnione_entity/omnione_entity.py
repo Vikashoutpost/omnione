2d9331b83d5ac6133d2abd3f1561e0d09f7e7439
anasoutpost <anas.outpost@gmail.com>
2026-04-08 04:25:30 +0000

    1st commit

--- omnione/omni_one/doctype/omnione_entity/omnione_entity.py
+++ omnione/omni_one/doctype/omnione_entity/omnione_entity.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2026, Outpost.Work LLP and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class OmnioneEntity(Document):
